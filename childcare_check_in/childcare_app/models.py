98e7ded60d592d183396364e4796b7f04ef1a0f3
tdhuynh <tommyhuynh93@gmail.com>
2016-11-03 14:43:50 -0400

    created models for app

--- childcare_check_in/childcare_app/models.py
+++ childcare_check_in/childcare_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+
+ACCESS_LEVEL = [
+    ('p', 'Parent'),
+    ('s', 'Staff')
+]
+
+@receiver(post_save, sender='auth.User')
+def create_user_profile(**kwargs):
+    created = kwargs.get('created')
+    instance = kwargs.get('instance')
+    if created:
+        Profile.objects.create(user=instance)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User')
+    access_level = models.CharField(max_length=1, choices=TYPE)
+
+
+class Child(models.Model):
+    first_name = models.CharField(max_length="15")
+    last_name = models.CharField(max_length="25")
+    parent = models.ForeignKey('auth.User')
+    on_site = models.BooleanField(default=False)
+    check_in = models.DateTimeField(auto_now=True)
+    check_out = models.DateTimeField(auto_now=True)
+
+
+class Pin(models.Model):
+    child = models.OneToOneField(Child)
+    number = models.IntegerField()
