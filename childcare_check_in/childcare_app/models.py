cd4fb7fe634f10a74bdbd6c902d06e0bbfbb577b
tdhuynh <tommyhuynh93@gmail.com>
2016-11-03 16:59:03 -0400

    made changes to models

--- childcare_check_in/childcare_app/models.py
+++ childcare_check_in/childcare_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 ACCESS_LEVEL = [
@@ -12,23 +13,19 @@ def create_user_profile(**kwargs):
     created = kwargs.get('created')
     instance = kwargs.get('instance')
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(user=instance, access_level='p')
 
 
 class Profile(models.Model):
     user = models.OneToOneField('auth.User')
-    access_level = models.CharField(max_length=1, choices=TYPE)
+    access_level = models.CharField(max_length=1, choices=ACCESS_LEVEL)
 
 
 class Child(models.Model):
-    first_name = models.CharField(max_length="15")
-    last_name = models.CharField(max_length="25")
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=25)
     parent = models.ForeignKey('auth.User')
     on_site = models.BooleanField(default=False)
     check_in = models.DateTimeField(auto_now=True)
     check_out = models.DateTimeField(auto_now=True)
-
-
-class Pin(models.Model):
-    child = models.OneToOneField(Child)
-    number = models.IntegerField()
+    code = models.CharField(max_length=4, unique=True)
--- childcare_check_in/childcare_app/views.py
+++ childcare_check_in/childcare_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm, User
+from django.urls import reverse_lazy
+
+from childcare_app.models import Child, Profile
+
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy("index_view")
+
+
+class ChildCreateView(CreateView):
+    model = Child
+    success_url = reverse_lazy("index_view")
+    fields = ('first_name', 'last_name', 'parent')
+
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.user = self.request.user
+        
